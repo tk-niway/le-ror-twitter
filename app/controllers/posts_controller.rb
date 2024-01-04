fde01149663052dd5f24f9ec0d778d95b0aefc8c
tk_niway <urban.hang274@gmail.com>
2024-01-04 20:14:26 +0900

    implemented delete api and created the related file of the feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,10 +12,26 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     post = Post.new(post_params)
     post.save
     redirect_to post_path(post.id)
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
