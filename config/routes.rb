e9a4d2ef9ba67cc3599a8146ac770ba128eec9c7
tk_niway <urban.hang274@gmail.com>
2024-01-04 23:29:52 +0900

    modified sytle for each post pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
 
-  get "top" => "homes#top"
+  root :to => 'homes#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
