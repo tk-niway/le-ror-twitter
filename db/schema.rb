007c82e84193b441be159213fa1b019065f9888a
tk_niway <urban.hang274@gmail.com>
2024-01-04 20:26:39 +0900

    added image_id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_03_095600) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_04_112304) do
   create_table "posts", force: :cascade do |t|
     t.string "location"
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
