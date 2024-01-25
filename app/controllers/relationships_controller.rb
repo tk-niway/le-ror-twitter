3cc0a0965da535fb527ece1febc5a4bfd9a68ee4
tk_niway <urban.hang274@gmail.com>
2024-01-26 02:26:00 +0900

    removed unnecessary syntax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,16 +8,4 @@ class RelationshipsController < ApplicationController
     current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end
-
-  def follows
-    user = User.find(params[:id])
-    logger.debug("user.follows: #{user.followed}")
-
-    @users = user.followed.page(params[:page]).per(3).reverse_order
-  end
-
-  def followers
-    user = User.find(params[:id])
-    @users = user.follower_user.page(params[:page]).per(3).reverse_order
-  end
 end
